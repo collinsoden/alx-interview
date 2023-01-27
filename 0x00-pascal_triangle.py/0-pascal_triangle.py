f7affeed4e4e5c4e66f6c3144dc09cba6b97b13a
Collins Oden <103268967+Collinsoden22@users.noreply.github.com>
2023-01-27 21:37:21 +0100

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle.py/0-pascal_triangle.py
+++ 0x00-pascal_triangle.py/0-pascal_triangle.py
@@ -1,4 +1,11 @@
+
+#!/usr/bin/python3
+'''
+Returns list representing the Pascal's triangle of n
+'''
+
 def pascal_triangle(n):
+   '''returns empty list if n <= 0'''
     if n <= 0:
         return []
     triangle = [[1]]
