8d1809a92af8107e183009cb394948573445507a
Collins Oden <103268967+Collinsoden22@users.noreply.github.com>
2023-02-12 16:48:21 +0100

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,18 +1,20 @@
-#usr/bin/python3
-def minOperations(n): 
-    if n == 0: 
-        return 0 
-    elif n == 1: 
-        return 1 
-    else: 
-        # find the largest power of 2 that is less than or equal to n 
-        p = 2  
-        while p <= n:  
-            p *= 2  
+#!/usr/bin/python3
+"""
+Contains a method that calculates the fewest number of operations
+needed to result in exactly n H characters in the file.
+"""
 
-        # subtract the largest power of 2 from n and recur for the remaining number  
-        return 1 + minOperations(n - (p//2))  
 
-     # Driver code  
-n = 10     # Number of H's required in the file  
-print(minOperations(n))
+def minOperations(n):
+    """
+    Returns the fewest number of operations needed to result in exactly
+    n H characters in the file.
+    """
+    operations = 0
+    min_operations = 2
+    while n > 1:
+        while n % min_operations == 0:
+            operations += min_operations
+            n /= min_operations
+        min_operations += 1
+    return operations
