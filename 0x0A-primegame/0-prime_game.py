04e911da438184209ac3488f32656ca75b9b3960
Collins Oden <103268967+Collinsoden22@users.noreply.github.com>
2023-04-03 22:53:18 +0100

    Cracked code for prime-games interview

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,36 @@
+def isWinner(x, nums):
+    wins = {"Maria": 0, "Ben": 0}
+    for n in nums:
+        winner = None
+        primes = set(range(2, n+1))
+        while primes:
+            # Maria's turn
+            if winner != "Ben":
+                m_pick = None
+                for p in sorted(primes):
+                    if all(p*i not in primes for i in range(2, n//p+1)):
+                        m_pick = p
+                        break
+                if m_pick is None:
+                    winner = "Ben"
+                    break
+                primes -= set(range(m_pick, n+1, m_pick))
+            # Ben's turn
+            if winner != "Maria":
+                b_pick = None
+                for p in sorted(primes):
+                    if all(p*i not in primes for i in range(2, n//p+1)):
+                        b_pick = p
+                        break
+                if b_pick is None:
+                    winner = "Maria"
+                    break
+                primes -= set(range(b_pick, n+1, b_pick))
+        if winner is not None:
+            wins[winner] += 1
+    if wins["Maria"] > wins["Ben"]:
+        return "Maria"
+    elif wins["Maria"] < wins["Ben"]:
+        return "Ben"
+    else:
+        return None
